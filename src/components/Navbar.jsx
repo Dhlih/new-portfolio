663e73a3e3986c3770b4179361fadf8b2272c5f2
ifad <yusuffadhlih123@gmail.com>
2025-03-24 16:51:51 +0700

    modified navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,29 +11,28 @@ const Navbar = () => {
   ];
 
   return (
-    <div className="fixed top-0 left-0 right-0 z-10 px-[2rem]">
-      <div className="flex justify-center">
-        <div className="w-fit max-w-[380px] rounded-full bg-[#1B1B1B]/85 backdrop-blur py-3 mt-8  outline-1 outline-[#424242]">
-          <ul className="space-x-7 flex items-center justify-center ">
-            {navItems.map((item) => (
-              <li key={item.path}>
-                <Link
-                  to={item.path}
-                  smooth={true}
-                  duration={500}
-                  onClick={() => {
-                    setIsActive(item.name);
-                  }}
-                  className={`cursor-pointer text-sm ${
-                    isActive === item.name ? "text-[#3CD98E]" : "text-white"
-                  }`}
-                >
-                  {item.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className="fixed top-0 right-0 left-0 z-10 px-[2rem]">
+      <div className="md:max-w-[380px] w-full mx-auto rounded-full bg-[#1B1B1B]/85 backdrop-blur py-3 mt-[2rem] outline-1 outline-[#424242] ">
+        <ul className="space-x-7 flex items-center justify-center">
+          {navItems.map((item) => (
+            <li key={item.path}>
+              <Link
+                to={item.path}
+                smooth={true}
+                duration={500}
+                onClick={() => {
+                  setIsActive(item.name);
+                  console.log(isActive);
+                }}
+                className={`cursor-pointer text-sm  ${
+                  isActive === item.name ? "text-[#3CD98E]" : "text-white"
+                }`}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
